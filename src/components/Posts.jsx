3220ab1841ac7d9cfb40b78375d0d9d2a26635de
karam mustafa <karam2mustafa@gmail.com>
2023-08-30 13:45:20 +0300

    wip

--- src/components/Posts.jsx
+++ src/components/Posts.jsx
@@ -17,194 +17,12 @@ export default function Posts() {
       created_at: '2023-08-25T06:17:47.000000Z',
       updated_at: '2023-08-25T06:17:47.000000Z',
     },
-    {
-      id: 1,
-      title: 'karam1',
-      description: 'karam1',
-      category_id: '1',
-      image:
-        'https://www.thaqfny.com/wp-content/uploads/2021/02/%D8%A7%D9%83%D8%AA%D8%A8-%D8%A7%D8%B3%D9%85%D9%83-%D8%B9%D9%84%D9%89-%D8%B5%D9%88%D8%B1-%D8%B5%D8%A8%D8%A7%D8%AD-%D8%A7%D9%84%D8%AE%D9%8A%D8%B1-3.jpeg',
-      link: 'karam1',
-      start_date: '2023-08-16 00:00:00',
-      end_date: '2023-08-31 00:00:00',
-      created_at: '2023-08-25T06:17:47.000000Z',
-      updated_at: '2023-08-25T06:17:47.000000Z',
-    },
-    {
-      id: 1,
-      title: 'karam1',
-      description: 'karam1',
-      category_id: '1',
-      image:
-        'https://www.thaqfny.com/wp-content/uploads/2021/02/%D8%A7%D9%83%D8%AA%D8%A8-%D8%A7%D8%B3%D9%85%D9%83-%D8%B9%D9%84%D9%89-%D8%B5%D9%88%D8%B1-%D8%B5%D8%A8%D8%A7%D8%AD-%D8%A7%D9%84%D8%AE%D9%8A%D8%B1-3.jpeg',
-      link: 'karam1',
-      start_date: '2023-08-16 00:00:00',
-      end_date: '2023-08-31 00:00:00',
-      created_at: '2023-08-25T06:17:47.000000Z',
-      updated_at: '2023-08-25T06:17:47.000000Z',
-    },
-    {
-      id: 1,
-      title: 'karam1',
-      description: 'karam1',
-      category_id: '1',
-      image:
-        'https://www.thaqfny.com/wp-content/uploads/2021/02/%D8%A7%D9%83%D8%AA%D8%A8-%D8%A7%D8%B3%D9%85%D9%83-%D8%B9%D9%84%D9%89-%D8%B5%D9%88%D8%B1-%D8%B5%D8%A8%D8%A7%D8%AD-%D8%A7%D9%84%D8%AE%D9%8A%D8%B1-3.jpeg',
-      link: 'karam1',
-      start_date: '2023-08-16 00:00:00',
-      end_date: '2023-08-31 00:00:00',
-      created_at: '2023-08-25T06:17:47.000000Z',
-      updated_at: '2023-08-25T06:17:47.000000Z',
-    },
-    {
-      id: 1,
-      title: 'karam1',
-      description: 'karam1',
-      category_id: '1',
-      image:
-        'https://www.thaqfny.com/wp-content/uploads/2021/02/%D8%A7%D9%83%D8%AA%D8%A8-%D8%A7%D8%B3%D9%85%D9%83-%D8%B9%D9%84%D9%89-%D8%B5%D9%88%D8%B1-%D8%B5%D8%A8%D8%A7%D8%AD-%D8%A7%D9%84%D8%AE%D9%8A%D8%B1-3.jpeg',
-      link: 'karam1',
-      start_date: '2023-08-16 00:00:00',
-      end_date: '2023-08-31 00:00:00',
-      created_at: '2023-08-25T06:17:47.000000Z',
-      updated_at: '2023-08-25T06:17:47.000000Z',
-    },
-    {
-      id: 1,
-      title: 'karam1',
-      description: 'karam1',
-      category_id: '1',
-      image:
-        'https://www.thaqfny.com/wp-content/uploads/2021/02/%D8%A7%D9%83%D8%AA%D8%A8-%D8%A7%D8%B3%D9%85%D9%83-%D8%B9%D9%84%D9%89-%D8%B5%D9%88%D8%B1-%D8%B5%D8%A8%D8%A7%D8%AD-%D8%A7%D9%84%D8%AE%D9%8A%D8%B1-3.jpeg',
-      link: 'karam1',
-      start_date: '2023-08-16 00:00:00',
-      end_date: '2023-08-31 00:00:00',
-      created_at: '2023-08-25T06:17:47.000000Z',
-      updated_at: '2023-08-25T06:17:47.000000Z',
-    },
-    {
-      id: 1,
-      title: 'karam1',
-      description: 'karam1',
-      category_id: '1',
-      image:
-        'https://www.thaqfny.com/wp-content/uploads/2021/02/%D8%A7%D9%83%D8%AA%D8%A8-%D8%A7%D8%B3%D9%85%D9%83-%D8%B9%D9%84%D9%89-%D8%B5%D9%88%D8%B1-%D8%B5%D8%A8%D8%A7%D8%AD-%D8%A7%D9%84%D8%AE%D9%8A%D8%B1-3.jpeg',
-      link: 'karam1',
-      start_date: '2023-08-16 00:00:00',
-      end_date: '2023-08-31 00:00:00',
-      created_at: '2023-08-25T06:17:47.000000Z',
-      updated_at: '2023-08-25T06:17:47.000000Z',
-    },
-    {
-      id: 1,
-      title: 'karam1',
-      description: 'karam1',
-      category_id: '1',
-      image:
-        'https://www.thaqfny.com/wp-content/uploads/2021/02/%D8%A7%D9%83%D8%AA%D8%A8-%D8%A7%D8%B3%D9%85%D9%83-%D8%B9%D9%84%D9%89-%D8%B5%D9%88%D8%B1-%D8%B5%D8%A8%D8%A7%D8%AD-%D8%A7%D9%84%D8%AE%D9%8A%D8%B1-3.jpeg',
-      link: 'karam1',
-      start_date: '2023-08-16 00:00:00',
-      end_date: '2023-08-31 00:00:00',
-      created_at: '2023-08-25T06:17:47.000000Z',
-      updated_at: '2023-08-25T06:17:47.000000Z',
-    },
-    {
-      id: 1,
-      title: 'karam1',
-      description: 'karam1',
-      category_id: '1',
-      image:
-        'https://www.thaqfny.com/wp-content/uploads/2021/02/%D8%A7%D9%83%D8%AA%D8%A8-%D8%A7%D8%B3%D9%85%D9%83-%D8%B9%D9%84%D9%89-%D8%B5%D9%88%D8%B1-%D8%B5%D8%A8%D8%A7%D8%AD-%D8%A7%D9%84%D8%AE%D9%8A%D8%B1-3.jpeg',
-      link: 'karam1',
-      start_date: '2023-08-16 00:00:00',
-      end_date: '2023-08-31 00:00:00',
-      created_at: '2023-08-25T06:17:47.000000Z',
-      updated_at: '2023-08-25T06:17:47.000000Z',
-    },
-    {
-      id: 1,
-      title: 'karam1',
-      description: 'karam1',
-      category_id: '1',
-      image:
-        'https://www.thaqfny.com/wp-content/uploads/2021/02/%D8%A7%D9%83%D8%AA%D8%A8-%D8%A7%D8%B3%D9%85%D9%83-%D8%B9%D9%84%D9%89-%D8%B5%D9%88%D8%B1-%D8%B5%D8%A8%D8%A7%D8%AD-%D8%A7%D9%84%D8%AE%D9%8A%D8%B1-3.jpeg',
-      link: 'karam1',
-      start_date: '2023-08-16 00:00:00',
-      end_date: '2023-08-31 00:00:00',
-      created_at: '2023-08-25T06:17:47.000000Z',
-      updated_at: '2023-08-25T06:17:47.000000Z',
-    },
-    {
-      id: 1,
-      title: 'karam1',
-      description: 'karam1',
-      category_id: '1',
-      image:
-        'https://www.thaqfny.com/wp-content/uploads/2021/02/%D8%A7%D9%83%D8%AA%D8%A8-%D8%A7%D8%B3%D9%85%D9%83-%D8%B9%D9%84%D9%89-%D8%B5%D9%88%D8%B1-%D8%B5%D8%A8%D8%A7%D8%AD-%D8%A7%D9%84%D8%AE%D9%8A%D8%B1-3.jpeg',
-      link: 'karam1',
-      start_date: '2023-08-16 00:00:00',
-      end_date: '2023-08-31 00:00:00',
-      created_at: '2023-08-25T06:17:47.000000Z',
-      updated_at: '2023-08-25T06:17:47.000000Z',
-    },
-    {
-      id: 1,
-      title: 'karam1',
-      description: 'karam1',
-      category_id: '1',
-      image:
-        'https://www.thaqfny.com/wp-content/uploads/2021/02/%D8%A7%D9%83%D8%AA%D8%A8-%D8%A7%D8%B3%D9%85%D9%83-%D8%B9%D9%84%D9%89-%D8%B5%D9%88%D8%B1-%D8%B5%D8%A8%D8%A7%D8%AD-%D8%A7%D9%84%D8%AE%D9%8A%D8%B1-3.jpeg',
-      link: 'karam1',
-      start_date: '2023-08-16 00:00:00',
-      end_date: '2023-08-31 00:00:00',
-      created_at: '2023-08-25T06:17:47.000000Z',
-      updated_at: '2023-08-25T06:17:47.000000Z',
-    },
-    {
-      id: 1,
-      title: 'karam1',
-      description: 'karam1',
-      category_id: '1',
-      image:
-        'https://www.thaqfny.com/wp-content/uploads/2021/02/%D8%A7%D9%83%D8%AA%D8%A8-%D8%A7%D8%B3%D9%85%D9%83-%D8%B9%D9%84%D9%89-%D8%B5%D9%88%D8%B1-%D8%B5%D8%A8%D8%A7%D8%AD-%D8%A7%D9%84%D8%AE%D9%8A%D8%B1-3.jpeg',
-      link: 'karam1',
-      start_date: '2023-08-16 00:00:00',
-      end_date: '2023-08-31 00:00:00',
-      created_at: '2023-08-25T06:17:47.000000Z',
-      updated_at: '2023-08-25T06:17:47.000000Z',
-    },
-    {
-      id: 1,
-      title: 'karam1',
-      description: 'karam1',
-      category_id: '1',
-      image:
-        'https://www.thaqfny.com/wp-content/uploads/2021/02/%D8%A7%D9%83%D8%AA%D8%A8-%D8%A7%D8%B3%D9%85%D9%83-%D8%B9%D9%84%D9%89-%D8%B5%D9%88%D8%B1-%D8%B5%D8%A8%D8%A7%D8%AD-%D8%A7%D9%84%D8%AE%D9%8A%D8%B1-3.jpeg',
-      link: 'karam1',
-      start_date: '2023-08-16 00:00:00',
-      end_date: '2023-08-31 00:00:00',
-      created_at: '2023-08-25T06:17:47.000000Z',
-      updated_at: '2023-08-25T06:17:47.000000Z',
-    },
-    {
-      id: 1,
-      title: 'karam1',
-      description: 'karam1',
-      category_id: '1',
-      image:
-        'https://www.thaqfny.com/wp-content/uploads/2021/02/%D8%A7%D9%83%D8%AA%D8%A8-%D8%A7%D8%B3%D9%85%D9%83-%D8%B9%D9%84%D9%89-%D8%B5%D9%88%D8%B1-%D8%B5%D8%A8%D8%A7%D8%AD-%D8%A7%D9%84%D8%AE%D9%8A%D8%B1-3.jpeg',
-      link: 'karam1',
-      start_date: '2023-08-16 00:00:00',
-      end_date: '2023-08-31 00:00:00',
-      created_at: '2023-08-25T06:17:47.000000Z',
-      updated_at: '2023-08-25T06:17:47.000000Z',
-    },
   ];
   return (
     <View>
       {data.map((item, index) => {
         return (
-          <View style={styles.post}>
+          <View style={styles.post} key={index}>
             <Image style={styles.image} source={{uri: item.image}} />
             <View
               style={{
@@ -213,17 +31,38 @@ export default function Posts() {
                 flexDirection: 'column',
                 alignItems: 'flex-end',
               }}>
-              <Text>{item.title}</Text>
-              <Text>
+              <Text
+                style={{
+                  fontSize: 20,
+                  fontWeight: 'bold',
+                  color: '#16ab75',
+                  marginBottom: 5,
+                }}>
+                {item.title}
+              </Text>
+              <Text style={{fontSize: 15, fontWeight: 800}}>
                 {item.description.substring(0, 200)}
                 {item.description.length > 200 ? ' ...' : ''}
               </Text>
               <View style={styles.dateArea}>
-                <Text> حتى {item.end_date}</Text>
-                <Text>تبدأ في {item.start_date}</Text>
+                <Text style={{fontSize: 15, fontWeight: 500}}>
+                  حتى {item.end_date}
+                </Text>
+                <Text style={{fontSize: 15, fontWeight: 500}}>
+                  تبدأ في {item.start_date}
+                </Text>
               </View>
               <View style={styles.link}>
-                <Text> {item.link}</Text>
+                <Text
+                  style={{
+                    fontSize: 20,
+                    fontWeight: 'bold',
+                    color: '#16ab75',
+                    marginBottom: 5,
+                  }}>
+                  {' '}
+                  {item.link}
+                </Text>
               </View>
             </View>
           </View>
@@ -249,11 +88,13 @@ const styles = StyleSheet.create({
   dateArea: {
     flexDirection: 'row',
     alignItems: 'center',
+    marginTop: 8,
   },
   link: {
     flexDirection: 'row',
     justifyContent: 'flex-start',
     alignItems: 'center',
-    width: '100%'
+    width: '100%',
+    marginTop: 8,
   },
 });
